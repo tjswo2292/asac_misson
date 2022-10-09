7d8bcc9ecc0d9f12b3a05464dccc4ce874955f1d
tjswo2292 <tjswo2292@naver.com>
2022-10-10 00:38:11 +0900

    10.9

---
+++ mission1/index.js
@@ -0,0 +1,12 @@
+const modalBtn = document.querySelector(".bar-icon-btn");
+const show = document.querySelector(".dimmed");
+
+function showmodal() {
+  show.style.display = "block";
+}
+function hideModal() {
+  show.style.display = "none";
+}
+
+modalBtn.addEventListener("mouseover", showmodal);
+modalBtn.addEventListener("mouseout", hideModal);
