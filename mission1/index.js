55466d0042a74e30d9b68eb0d5bedf1ed4e85a64
tjswo2292 <tjswo2292@naver.com>
2022-10-11 20:22:50 +0900

    이미지 슬라이드 구현 중

--- mission1/index.js
+++ mission1/index.js
@@ -1,6 +1,16 @@
 const modalBtn = document.querySelector(".bar-icon-btn");
 const show = document.querySelector(".dimmed");
 
+const $prevBtn = document.querySelector(".prev-img-btn");
+const $nextBtn = document.querySelector(".next-img-btn");
+const $imgBox = document.querySelector(".main-img-box");
+
+// Image slide
+function moveImg() {
+  $imgBox.style.transform = "translateX(-1200px)";
+  $imgBox.style.transition = "all .5s ease-in-out";
+}
+// Modal
 function showmodal() {
   show.style.display = "block";
 }
@@ -10,3 +20,7 @@ function hideModal() {
 
 modalBtn.addEventListener("mouseover", showmodal);
 modalBtn.addEventListener("mouseout", hideModal);
+
+$prevBtn.addEventListener("click", moveImg);
+
+// 넘어가는 사진은 안보이게 해주면 됨
